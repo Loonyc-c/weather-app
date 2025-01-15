ea93a844ed221a3fa742e715ae48dafe5d0a6c91
Battulga <battulgabathuyg54@gmail.com>
2025-01-16 01:28:31 +0800

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import moon from "./assets/moon-little.webp";
 import sun from "./assets/sun-little.webp";
 import { useEffect, useState } from "react";
-import countriesData from "./data.jsx";
 import Search from "./components/search.jsx";
 import WeatherCard from "./components/weather-card.jsx";
 import MiddleCircle from "./components/middle-circle.jsx";
@@ -11,9 +10,11 @@ function App() {
   const [searchValue, setSearchValue] = useState("");
   const [filteredData, setFilteredData] = useState([]);
   const [allCities, setAllCities] = useState([]);
-  const [weatherData, setWeatherData] = useState([])
+  const [weatherData, setWeatherData] = useState({})
   const [selectedCity, setSelectedCity] = useState("Ulan Bator, Mongolia")
-   
+  
+console.log(`this is data weatherData`)
+
  console.log(weatherData)
   const getCountries = async() => {
     try{  
@@ -47,15 +48,12 @@ function App() {
     getWeatherData()
   },[selectedCity])
 
+  const handleClickCity = (city)=>{
+    setSelectedCity(city)
+    setSearchValue("")
+    setFilteredData([])
+  }
   // 
-
-  // useEffect(() => {
-  //   const cities = countriesData.flatMap((obj) =>
-  //     obj.cities.map((city) => `${city}, ${obj.country}`)
-  //   );
-  //   console.log("all cities", cities);
-  //   setAllCities(cities);
-  // }, [countriesData]);
 
   const handleSearchChange = (e) => {
     setSearchValue(e.target.value);
@@ -72,16 +70,21 @@ function App() {
         {/* left side */}
         <div className=" w-screen h-screen bg-#F3F4F6 justify-center items-center flex flex-col">
           {/* search input */}
-          <Search searchValue={searchValue} handleSearchChange={handleSearchChange} filteredData={filteredData}/>
+          <Search searchValue={searchValue} 
+          handleSearchChange={handleSearchChange} 
+          filteredData={filteredData}
+          handleClickCity={handleClickCity}
+          
+          />
 
           {/* day */}
           <WeatherCard
-            date="date"
-            city="City Name"
-            temp={26}
+            date={weatherData.forecast?.forecastday[0]?.date}
+            city={weatherData.location?.name}
+            temp={weatherData.forecast?.forecastday[0]?.day?.maxtemp_c}
             imageSrc={`https://s3-alpha-sig.figma.com/img/695d/d5ec/821b86dd320b93434750d28a17b16fc0?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=fdLdzS3dxr0-t5SiN2fDMkxayH-uq0A8msh4wXcpSveM5sDN0IZ~-K0m4uXWVsAV9DGkPQDJlyndEKFu7JZVqpKMOfi2fKHKS642HII2MgKwOBa5pNxqk2xh7TsJ0m~zwXlrztxu7DETzSsYn3yV25dzlvEZjXct9h1vafyp6jAjUzdPEFopW3Wfe6jk90gzgX2lNYXPEv6lj0VdgQvVBqAK-PN~-ShY8nHV6JQ4x1YhX0UDeb44ulngf4b3kgrzJZ~DNiY8A5wCSFP~pOboVuBAc~OzVVPh6m5HnFjIc3VHXdbSgcGpYpyeqmuQZ9oJbSEYaPB~B4M7YvxekKfPCg__`}
             dayOrNight="day"
-            weatherContidtion= "sunny" 
+            weatherContidtion={weatherData.forecast?.forecastday[0]?.day?.condition?.text}
             cornerCircle={sun} />
         </div>
         {/* right side  */}
@@ -89,12 +92,12 @@ function App() {
         <div className="w-screen h-screen bg-[#0F141E] justify-center items-center flex flex-col">
           {/* night */}
           <WeatherCard
-            date="date"
-            city="City Name"
-            temp={26}
+            date={weatherData.forecast?.forecastday[0]?.date}
+            city={weatherData.location?.name}
+            temp={weatherData.forecast?.forecastday[0]?.day?.mintemp_c}
             imageSrc={`https://s3-alpha-sig.figma.com/img/b6fe/b523/f01b7c0c0765dab6de4f9f5cbb022e1d?Expires=1737936000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Ft9~h2kAcRihnxvVgA9fussyynRP2N5P0~9iKCQRv-isLrnzSbeXK~uwJwozaftBvHpqV4ba8sRJGIueVAlZd2pKb3A05AyUdcGMzjnnAPNoA-vyKiLzxjVpXeinrFUUEJw9iQOY279drc9PU5iicGoLQVC9RkCfDtT9ta5AfBjwtQWxcEfRmR8dcXEkIOJ1IGF8GL5infm6xZb5Pej7C7bIllsp-mPuUiK11NOvhnG-AtGn1HeAJtO7VPY-zO2ih0zc6N7kbnPtAMmsum2OdgvwaKi5mi1~fJBlq77R9d5a6jjUzBRihyLqHDLlofC32fyYVclAlvT2c9odRKxooQ__`}
             dayOrNight="harangui" 
-            weatherContidtion="night"
+            weatherContidtion={weatherData.forecast?.forecastday[0]?.day?.condition?.text}
             cornerCircle={moon} />
         </div>
 
--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,8 +1,9 @@
 import SearchIcon from "../icons/search";
 import LocationIcon from "../icons/location-icon";
 function Search(props) {
-  const { searchValue, handleSearchChange, filteredData } = props;
+  const { searchValue, handleSearchChange, filteredData,handleClickCity } = props;
   const shouldDisplay = searchValue.length > 0 && filteredData.length > 0;
+  
   return (
     <div className="absolute mt-[-800px] w-[500px] h-[80px] gap-[30px] z-20 flex flex-col  rounded-full bg-[#ffff]">
       <div className="relative items-center flex rounded-full pl-[30px] bg-[#ffff]">
@@ -16,12 +17,13 @@ function Search(props) {
         />
       </div>
      { shouldDisplay && (<div className="rounded-3xl bg-white/80 py-4 shadow-lg backdrop-blur-md">
-          {filteredData.map((city, index) => (
+          {filteredData.map((el) => (
             <p
               className="flex text-[18px] gap-[10px] px-[10px] py-[5px]"
-              key={index}
+              key={el}
+              onClick={()=>handleClickCity(el)}
             >
-              <LocationIcon /> {city}
+              <LocationIcon /> {el}
             </p>
           ))}
         </div>)
